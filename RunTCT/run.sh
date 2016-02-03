76b71df9938b885badbce6e678e2ff60a623073d
Yoonsang Lee <yssl.ssl@gmail.com>
2016-02-03 10:10:55 +0900

    Update run.sh

--- RunTCT/run.sh
+++ RunTCT/run.sh
@@ -1,11 +1,11 @@
 # run dali-core utc
-stdbuf -i0 -o0 -e0 python ./runtct-main.py utc dali-core
+stdbuf -i0 -o0 -e0 ./runtct.py utc dali-core
 
 ## run dali-toolkit utc
-#stdbuf -i0 -o0 -e0 python ./runtct-main.py utc dali-toolkit
+#stdbuf -i0 -o0 -e0 ./runtct.py utc dali-toolkit
 
 ## run dali-core utc from 1278th tc function
-#stdbuf -i0 -o0 -e0 python ./runtct-main.py utc dali-core 1278
+#stdbuf -i0 -o0 -e0 ./runtct.py utc dali-core 1278
 
 ## run dali-core utc from 10th to 20th tc function
-#stdbuf -i0 -o0 -e0 python ./runtct-main.py utc dali-core 10 20
+#stdbuf -i0 -o0 -e0 ./runtct.py utc dali-core 10 20
